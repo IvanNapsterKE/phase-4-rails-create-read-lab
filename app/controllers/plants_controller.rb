7c1da66badece16503ae7fe64eac349830368eb7
IvanNapsterKE <nguyoivan@gmail.com>
2022-11-28 14:41:12 +0300

    Done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,37 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+      end
+    
+      # POST /plants
+      def create
+        plant = Plant.create(name: params[:name], species: params[:species])
+        render json: plant, status: :created
+      end
+    
+      # GET /plants/:id
+      def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+          render json: plant
+        else
+          render json: { error: "plant not found" }, status: :not_found
+        end
+      end
+    
+        # POST /plants
+        def create
+          plant = Plant.create(plant_params)
+          render json: plant, status: :created
+        end
+    
+        # other controller actions here
+    
+        private
+        # all methods below here are private
+    
+        def plant_params
+          params.permit(:name, :image, :price)
+        end
+end
